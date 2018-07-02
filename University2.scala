9eed3ac7b90b3443cb49477a39405045814c61c1
Jesús López-González <jesus.lopez@hablapps.com>
2018-07-02 15:43:52 +0200

    Adopt case classes for entities

--- University2.scala
+++ University2.scala
@@ -8,39 +8,22 @@ import scalaz._, Scalaz._
  * State algebras
  */
 
-trait Getter[P[_],S]{
-  def apply(): P[S]
+case class Getter[P[_],S](get: P[S]) {
+  def apply() = get
 }
 
-object Getter {
-  def apply[P[_], S](get: P[S]): Getter[P, S] =
-    new Getter[P, S] { def apply() = get }
+case class Setter[P[_],S](set: S => P[Unit]) {
+  def apply(s: S): P[Unit] = set(s)
 }
 
-trait Setter[P[_],S]{
-  def apply(t: S): P[Unit]
-}
-
-object Setter {
-  def apply[P[_], S](put: S => P[Unit]): Setter[P, S] =
-    new Setter[P, S] { def apply(s: S) = put(s) }
-}
-
-trait Field[P[_],S]{
-  val get: Getter[P,S]
-  val put: Setter[P,S]
-
+case class Field[P[_],S](get: Getter[P,S], put: Setter[P,S]) {
   def modify(f: S => S)(implicit B: Bind[P]): P[Unit] = 
     get() >>= { s => put(f(s)) } 
 }
 
-object Field {
-  def apply[P[_], S](getter: Getter[P, S], setter: Setter[P, S]) =
-    new Field[P, S] { val get = getter; val put = setter }
-}
+case class ListP[Alg[_[_],_], P[_], S](algs: P[List[Alg[P, S]]]) {
 
-trait ListP[Alg[_[_],_],P[_],S]{
-  val apply: P[List[Alg[P,S]]]
+  def apply() = algs
 
   def traverse[T](
       f: Alg[P,S] => P[T])(implicit 
@@ -54,11 +37,6 @@ trait ListP[Alg[_[_],_],P[_],S]{
     apply >>= { algs => 
       (algs zip values) traverse f.tupled
     }
-}
-
-object ListP {
-  def apply[Alg[_[_], _], P[_], S](ap: P[List[Alg[P, S]]]) =
-    new ListP[Alg, P, S] { val apply = ap }
 }
 
 trait StdTraverse[P[_],S] extends ListP[Field,P,S]{
@@ -81,10 +59,9 @@ object Util {
   object StateField {
 
     def apply[S, A](g: S => A, p: A => S => S): StateField[S, A] =
-      new Field[State[S, ?], A] {
-        val get = Getter(State.gets(g))
-        val put = Setter(a => State(s => (p(a)(s), ())))
-      }
+      Field[State[S, ?], A](
+        Getter(State.gets(g)),
+        Setter(a => State(s => (p(a)(s), ()))))
 
     def refl[S]: StateField[S, S] = apply(identity, const)
   }
@@ -104,10 +81,9 @@ object Util {
   // Lens & StateField are isomorphic
 
   def lens2StateField[S, A](ln: Lens[S, A]): StateField[S, A] =
-    new Field[State[S, ?], A] {
-      val get = Getter(ln(State.get)) 
-      val put = Setter(a => ln(State.put(a)))
-    }
+    Field[State[S, ?], A](
+      Getter(ln(State.get)),
+      Setter(a => ln(State.put(a))))
 
   def stateField2Lens[S, A](fl: StateField[S, A]): Lens[S, A] =
     λ[State[A, ?] ~> State[S, ?]] { sta =>
@@ -119,36 +95,49 @@ object Util {
   
   // Maps the interpretation of an algebra. 
   trait AlgFunctor[Alg[_[_], _]] {
-    def amap[Q[_], P[_]](f: Q ~> P): Alg[Q, ?] ~> Alg[P, ?]
+    def amap[Q[_]: Functor, P[_]: Functor](f: Q ~> P): Alg[Q, ?] ~> Alg[P, ?]
   }
 
   object AlgFunctor {
 
     // XXX: boilerplate instances, consider using Shapeless here?
     
-    implicit def GetterAlgFunctor = new AlgFunctor[Getter] {
-      def amap[Q[_], P[_]](f: Q ~> P) =
+    implicit val GetterAlgFunctor = new AlgFunctor[Getter] {
+      def amap[Q[_]: Functor, P[_]: Functor](f: Q ~> P) =
         λ[Getter[Q, ?] ~> Getter[P, ?]] { alg =>
           Getter(f(alg.apply))
         }
     }
 
-    implicit def SetterAlgFunctor = new AlgFunctor[Setter] {
-      def amap[Q[_], P[_]](f: Q ~> P) =
+    implicit val SetterAlgFunctor = new AlgFunctor[Setter] {
+      def amap[Q[_]: Functor, P[_]: Functor](f: Q ~> P) =
         λ[Setter[Q, ?] ~> Setter[P, ?]] { alg =>
           Setter(x => f(alg(x)))
         }
     }
 
-    implicit def FieldAlgFunctor = new AlgFunctor[Field] {
-      def amap[Q[_], P[_]](f: Q ~> P) =
+    implicit val FieldAlgFunctor = new AlgFunctor[Field] {
+      def amap[Q[_]: Functor, P[_]: Functor](f: Q ~> P) =
         λ[Field[Q, ?] ~> Field[P, ?]] { alg =>
-          Field(alg.get.amap(f), alg.put.amap(f))
+          // XXX: syntax isn't working properly here 
+          Field(
+            GetterAlgFunctor.amap(f)(implicitly, implicitly)(alg.get), 
+            SetterAlgFunctor.amap(f)(implicitly, implicitly)(alg.put))
         }
     }
+
+    implicit def ListPAlgFunctor[Alg[_[_], _] : AlgFunctor] =
+      new AlgFunctor[ListP[Alg, ?[_], ?]] {
+        def amap[Q[_]: Functor, P[_]: Functor](f: Q ~> P) =
+          λ[ListP[Alg, Q, ?] ~> ListP[Alg, P, ?]] { alg =>
+            ListP(f(alg.algs.map(_.map(_ amap f))))
+          }
+      }
     
-    implicit class AlgFunctorOps[Alg[_[_], _], Q[_], A](al: Alg[Q, A]) {
-      def amap[P[_]](f: Q ~> P)(implicit AF: AlgFunctor[Alg]) = AF.amap(f)(al)
+    implicit class AlgFunctorOps[Alg[_[_], _], Q[_]: Functor, A](
+        al: Alg[Q, A]) {
+      def amap[P[_]: Functor](f: Q ~> P)(implicit AF: AlgFunctor[Alg]) = 
+        AF.amap(f)(implicitly, implicitly)(al)
     }
   }
 }
@@ -168,43 +157,43 @@ object Primitive{
 
 import Primitive._
 
-trait Department[P[_],D]{
-  val self: Field[P,D]
-  val budget: IntegerP[P]
-}
+case class Department[P[_],D](
+  self: Field[P,D],
+  budget: IntegerP[P])
 
 object Department {
-  
-  def apply[P[_], D](se: Field[P, D], bu: IntegerP[P]): Department[P, D] =
-    new Department[P, D] {
-      val self = se
-      val budget = bu
-    }
-
-  implicit def DepartmentAlgFunctor = new AlgFunctor[Department] {
-    def amap[Q[_], P[_]](f: Q ~> P) =
+  implicit val DepartmentAlgFunctor = new AlgFunctor[Department] {
+    def amap[Q[_]: Functor, P[_]: Functor](f: Q ~> P) =
       λ[Department[Q, ?] ~> Department[P, ?]] { alg =>
         Department(alg.self.amap(f), alg.budget.amap(f))
       }
   }
 }
 
-trait University[P[_],U]{
-  type D
+case class University[P[_], U, D](
+  self: Field[P, U],
+  name: StringP[P],
+  deps: ListP[Department, P, D])
 
-  val self: Field[P,U]
-  val name: StringP[P]
-  val deps: ListP[Department,P,D]
+object University {
+  implicit def UniversityAlgFunctor[D] = 
+    new AlgFunctor[University[?[_], ?, D]] {
+      def amap[Q[_]: Functor, P[_]: Functor](f: Q ~> P) =
+        λ[University[Q, ?, D] ~> University[P, ?, D]] { alg =>
+          University(alg.self.amap(f), alg.name.amap(f), alg.deps.amap(f))
+        }
+    }
 }
 
-trait Logic[P[_],U]{
-  val univ: University[P,U]
+trait Logic[P[_], U, D]{
+
+  val univ: University[P, U, D]
 
   def doubleBudget(implicit M: Monad[P]): P[List[Unit]] =
     univ.deps traverse { _.budget.modify(_*2) }
   
   // should be reused
-  def getDepts(implicit M: Monad[P]): P[List[univ.D]] = 
+  def getDepts(implicit M: Monad[P]): P[List[D]] = 
     univ.deps traverse { _.self.get() }
 }
 
@@ -236,25 +225,21 @@ object SDepartment {
 import StateField.refl
 import SUniversity._, SDepartment._
 
-object StateDepartment extends Department[State[SDepartment, ?], SDepartment] {
-  val self   = refl
-  val budget = refl amap budgetLn
-}
-
-object StateUniversity extends University[State[SUniversity, ?], SUniversity] {
-  
-  type D = SDepartment
-
-  val self = refl 
-  val name = refl amap nameLn
-  val deps = ListP(State.gets(_.deps.toList.map { case (k, d) =>
-    StateDepartment amap depLn(k, d)
-  }))
-}
-
 object Main extends App {
 
-  val logic = new Logic[State[SUniversity, ?], SUniversity] {
+  val StateDepartment = Department[State[SDepartment, ?], SDepartment](
+    refl[SDepartment],
+    refl[Int] amap budgetLn)
+
+  val StateUniversity = 
+    University[State[SUniversity, ?], SUniversity, SDepartment](
+      refl[SUniversity], 
+      refl[String] amap nameLn,
+      ListP(State.gets(_.deps.toList.map { case (k, d) =>
+        StateDepartment amap depLn(k, d)
+      })))
+
+  val logic = new Logic[State[SUniversity, ?], SUniversity, SDepartment] {
     val univ = StateUniversity
   }
   
