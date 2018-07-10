4cc6096d0b2c47f55cc3c9c0cdccab83885348e8
Jesús López-González <jesus.lopez@hablapps.com>
2018-07-10 12:54:33 +0200

    Add (failing) test that evidences the issue

--- src/test/scala/University.scala
+++ src/test/scala/University.scala
@@ -58,6 +58,7 @@ object University {
 
 case class City[U, D, P[_], C](
   self: Field[P, C],
+  name: Field[P, String],
   popu: Field[P, Int],
   univ: University[D, P, U])
 
@@ -71,7 +72,11 @@ object City {
     new AlgFunctor[City[U, D, ?[_], ?]] {
       def amap[Q[_]: Functor, P[_]: Functor](f: Q ~> P) =
         λ[City[U, D, Q, ?] ~> City[U, D, P, ?]] { alg =>
-          City(alg.self.amap(f), alg.popu.amap(f), alg.univ.amap(f))
+          City(
+            alg.self.amap(f), 
+            alg.name.amap(f), 
+            alg.popu.amap(f), 
+            alg.univ.amap(f))
         }
     }
 }
@@ -87,6 +92,9 @@ case class Logic[P[_], C, U, D](city: City[U, D, P, C]) {
   // should be reused
   def getMathDep: P[D] = 
     city.univ.math.self.get()
+
+  def uppercaseUnivName(implicit M: Monad[P]): P[Unit] =
+    city.univ.name.modify(_.toUpperCase)
 }
 
 
@@ -94,7 +102,7 @@ case class Logic[P[_], C, U, D](city: City[U, D, P, C]) {
  * Data Layer Interpretation
  */
 
-case class SCity(popu: Int, univ: SUniversity)
+case class SCity(popu: Int, name: String, univ: SUniversity)
 case class SUniversity(name: String, math: SDepartment)
 case class SDepartment(budget: Int)
 
@@ -102,14 +110,6 @@ import org.scalatest._
 
 class UniversitySpec extends FlatSpec with Matchers {
   
-  import shapeless.syntax.singleton._
-
-  // implicit val self0 = 'self ->> Util.Lens[SCity, SUniversity](
-  //   _.univ, u2 => _.copy(univ = u2))
-  // implicit val self1 = 'self ->> Util.Lens[SUniversity, SDepartment](
-  //   _.math, d2 => _.copy(math = d2)) 
-  // implicit val self2 = 'self ->> (self0 compose self1)
-
   "Automagic instances" should "be generated for city" in {
   
     val StateCity = 
@@ -118,15 +118,19 @@ class UniversitySpec extends FlatSpec with Matchers {
     val logic = Logic(StateCity)
     
     val urjc = SUniversity("urjc", SDepartment(3000))
-    val most = SCity(200000, urjc)
+    val most = SCity(200000, "mostoles", urjc)
 
     val popu2 = logic.getPopu.eval(most)
     val most2 = logic.doubleBudget(implicitly).exec(most)
     val math2 = logic.getMathDep.eval(most2)
 
     popu2 should be (200000)
-    most2 should be (SCity(200000, SUniversity("urjc", SDepartment(6000))))
+    most2 should be (most.copy(univ = SUniversity("urjc", SDepartment(6000))))
     math2 should be (SDepartment(6000))
+
+    val most3 = logic.uppercaseUnivName.exec(most)
+
+    most3 should be (most.copy(univ = SUniversity("URJC", SDepartment(3000))))
   }
 
   it should "be generated for department" in {
