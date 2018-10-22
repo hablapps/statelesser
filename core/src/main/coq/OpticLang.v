31a9d601b06059d7715d081d54302d431b043932
Jesús López-González <jesus.lopez@hablapps.com>
2018-10-22 10:55:33 +0200

    Add optic ids, new examples and congruence usage

--- core/src/main/coq/OpticLang.v
+++ core/src/main/coq/OpticLang.v
@@ -1,8 +1,9 @@
 Require Import Program.Basics.
-Require Import Coq.Bool.Bool.
 Require Import Coq.Strings.String.
 Require Import Coq.Init.Specif.
 Require Import Coq.Vectors.VectorDef.
+Require Import Coq.Arith.PeanoNat.
+Require Import Coq.Init.Logic.
 
 Open Scope program_scope.
 
@@ -12,6 +13,17 @@ Record Lens S A := mkLens
 { get : S -> A
 ; put : S -> A -> S
 }.
+
+Arguments mkLens [S A].
+
+Definition idLn {S : Type} : Lens S S :=
+  mkLens id (fun _ s => s).
+
+Definition fstLn {A B : Type} : Lens (A * B) A :=
+  mkLens fst (fun ab a => (a, snd ab)).
+
+Definition sndLn {A B : Type} : Lens (A * B) B :=
+  mkLens snd (fun ab b => (fst ab, b)).
 
 Class Monoid (M : Type) :=
 { mempty : M
@@ -23,14 +35,22 @@ Record Fold (S A : Type) := mkFold
 
 Arguments mkFold [S A].
 
-Definition idFold {S : Type} : Fold S S :=
+Definition idFl {S : Type} : Fold S S :=
   mkFold (fun M _ f s => f s).
 
 Definition result S A (n : nat) : Type := 
   t A n * (t A n -> S).
 
 Record Traversal S A := mkTraversal
-{ extract : S -> {n & result S A n} }.
+{ extract : S -> sigT (result S A) }.
+
+Arguments mkTraversal [S A].
+
+Check existT.
+Check cons.
+
+Definition idTr {S : Type} : Traversal S S :=
+  mkTraversal (fun s => existT (result S S) 1 (cons S s 0 (nil S), hd)).
 
 (* Finally, an optic language *)
 
@@ -55,7 +75,7 @@ Class OpticLang (expr obs : Type -> Type) :=
 
   (* fold-related primitives *)
 ; fold : forall {S A : Type}, Fold S A -> expr (Fold S A)
-; filter : forall {S : Type}, (S -> bool) -> expr (Fold S S)
+; filter : forall {S : Type}, (S -> Prop) -> expr (Fold S S)
 ; flComposeHoriz : forall {S A B : Type},
     expr (Fold S A) -> expr (Fold S B) -> expr (Fold S (A * B))
 ; flComposeVerti : forall {S A B : Type},
@@ -74,7 +94,11 @@ Notation "fl1 +_fl fl2" := (flComposeVerti fl1 fl2) (at level 50, left associati
 Notation "fl1 *_fl fl2" := (flComposeHoriz fl1 fl2) (at level 40, left associativity).
 
 Class OpticLangOpt (expr obs : Type -> Type) `{OpticLang expr obs} :=
-{ flAssocV : forall S A B C (fl1 : expr (Fold S A)) (fl2 : expr (Fold A B)) (fl3 : expr (Fold B C)),
+{ flLeftId : forall S A (fl : expr (Fold S A)),
+    fold idFl +_fl fl = fl
+; flRightId : forall S A (fl : expr (Fold S A)),
+    fl +_fl fold idFl = fl
+; flAssocV : forall S A B C (fl1 : expr (Fold S A)) (fl2 : expr (Fold A B)) (fl3 : expr (Fold B C)),
     fl1 +_fl (fl2 +_fl fl3) = fl1 +_fl fl2 +_fl fl3
 ; trAsFoldDistH : forall S A B (tr1 : expr (Traversal S A)) (tr2 : expr (Traversal S B)),
     trAsFold (tr1 *_tr tr2) = trAsFold tr1 *_fl trAsFold tr2
@@ -84,14 +108,16 @@ Class OpticLangOpt (expr obs : Type -> Type) `{OpticLang expr obs} :=
     lnAsTraversal (ln1 *_ln ln2) = lnAsTraversal ln1 *_tr lnAsTraversal ln2
 ; lnAsTravDistV : forall S A B (ln1 : expr (Lens S A)) (ln2 : expr (Lens A B)),
     lnAsTraversal (ln1 +_ln ln2) = lnAsTraversal ln1 +_tr lnAsTraversal ln2
-; filterFilter : forall S (p q : S -> bool),
-    filter p +_fl filter q = filter (fun s => (p s) && (q s))
-; filterTrue : forall S, @filter _ _ _ S (Basics.const true) = fold idFold
-; filterProd : forall S A B (fl1 : expr (Fold S A)) (fl2 : expr (Fold S B)) (p : A -> bool),
+; filterFilter : forall S (p q : S -> Prop),
+    filter p +_fl filter q = filter (fun s => p s /\ q s)
+; filterTrue : forall S, @filter _ _ _ S (fun _ => True) = fold idFl
+; filterProd : forall S A B (fl1 : expr (Fold S A)) (fl2 : expr (Fold S B)) (p : A -> Prop),
     (fl1 +_fl filter p) *_fl fl2 = fl1 *_fl fl2 +_fl filter (p ∘ fst)
-; VertDistHoriz :
+; vertDistHoriz :
     forall S A B C (fl1 : expr (Fold S A)) (fl2 : expr (Fold A B)) (fl3 : expr (Fold A C)),
       fl1 +_fl (fl2 *_fl fl3) = (fl1 +_fl fl2) *_fl (fl1 +_fl fl3)
+; liftIdLn : forall S, lnAsTraversal (lens (@idLn S)) = traversal idTr
+; liftIdTr : forall S, trAsFold (traversal (@idTr S)) = fold idFl
 }.
 
 (* Couples example *)
@@ -134,7 +160,7 @@ Proof.
   intros.
   destruct H0.
   unfold getName, getNameN.
-  now rewrite trAsFoldDistV0.
+  congruence.
 Qed.
 
 (* Query [getAgeAndName] *)
@@ -152,7 +178,7 @@ Proof.
   intros.
   destruct H0.
   unfold getAgeAndName, getAgeAndNameN.
-  now rewrite lnAsTravDistH0, trAsFoldDistV0, trAsFoldDistH0, VertDistHoriz0.
+  congruence.
 Qed.
 
 (* Query [getHerAges] *)
@@ -171,5 +197,27 @@ Proof.
   intros.
   destruct H0.
   unfold getHerAges, getHerAgesN.
-  now rewrite trAsFoldDistV0, lnAsTravDistV0, trAsFoldDistV0, flAssocV0.
+  congruence.
+Qed.
+
+(* Query [getPeopleOnTheirThirties] *)
+
+Definition getPeopleOnTheirThirties `{OpticLang expr obs} : obs (list Person -> list Person) :=
+  flGetAll (trAsFold (traversal peopleTr +_tr lnAsTraversal (lens idLn *_ln lens ageLn)) +_fl
+            filter ((fun a => (30 <= a) /\ (a < 40)) ∘ snd) +_fl
+            trAsFold (lnAsTraversal (lens fstLn))).
+
+Definition getPeopleOnTheirThirtiesN `{OpticLang expr obs} : obs (list Person -> list Person) :=
+  flGetAll (trAsFold (traversal peopleTr) *_fl 
+             (trAsFold (traversal peopleTr) +_fl trAsFold (lnAsTraversal (lens ageLn))) +_fl
+            filter ((fun a => (30 <= a) /\ (a < 40)) ∘ snd) +_fl
+            trAsFold (lnAsTraversal (lens fstLn))).
+
+Example normalize_getPeopleOnTheirThirties : forall expr obs `{OpticLangOpt expr obs},
+  getPeopleOnTheirThirties = getPeopleOnTheirThirtiesN.
+Proof.
+  intros.
+  destruct H0.
+  unfold getPeopleOnTheirThirties, getPeopleOnTheirThirtiesN.
+  congruence.
 Qed.
