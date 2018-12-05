c44bacb0ab2bf710262e168e728b0dc2dea5bdb3
Jesús López-González <jesus.lopez@hablapps.com>
2018-12-05 16:48:28 +0100

    Avoid asInstanceOf by "landing types"?!?!

--- core/src/main/scala/ProductFirst.scala
+++ core/src/main/scala/ProductFirst.scala
@@ -6,25 +6,46 @@ object ProductFirst {
 
   case class Unk[E[_], A](e: E[A]) extends ProductFirst[E, A]
 
-  case class Product[E[_], S, A, B, C](
+  case class Product[E[_], S, A, B](
     l: E[Getter[S, A]], 
-    r: E[Getter[S, B]]) extends ProductFirst[E, Getter[S, C]]
+    r: E[Getter[S, B]]) extends ProductFirst[E, Getter[S, (A, B)]]
 
-  case class First[E[_], A, B, S]() 
-    extends ProductFirst[E, Getter[S, A]]
+  case class First[E[_], A, B](
+      d: E[Getter[(A, B), A]]) 
+    extends ProductFirst[E, Getter[(A, B), A]]
 
-  case class FirstAndThen[E[_], A, B, S, C](e: E[Getter[A, C]])
-    extends ProductFirst[E, Getter[S, C]]
+  case class FirstAndNext[E[_], A, B, C](
+      d: E[Getter[(A, B), A]], 
+      e: E[Getter[A, C]])
+    extends ProductFirst[E, Getter[(A, B), C]]
 
   def run[E[_], A](pf: ProductFirst[E, A])(implicit ev: OpticLang[E]): E[A] =
     pf match {
       case Unk(e) => e
       case Product(l, r) => ev.gtHori(l, r)
-      case First() => ev.first
-      case FirstAndThen(e) => ev.gtVert(ev.first, e)
+      case First(_) => ev.first
+      case FirstAndNext(_, e) => ev.gtVert(ev.first, e)
     }
 
-  implicit def pfOpticLang[E[_]: OpticLang] = new OpticLang[ProductFirst[E, ?]] {
+  // Landing types with Scala, so beautiful! </ironic>
+
+  def landing1[E[_]: OpticLang, A, B, C](
+      d: E[Getter[(A, B), A]],
+      e: ProductFirst[E, Getter[A, C]]): FirstAndNext[E, A, B, C] = 
+    FirstAndNext[E, A, B, C](OpticLang[E].first, run(e))
+    
+  def landing2[E[_]: OpticLang, A, B, C, D](
+      d: E[Getter[(A, B), A]],
+      e: E[Getter[A, C]],
+      p: ProductFirst[E, Getter[C, D]]): FirstAndNext[E, A, B, D] =
+    FirstAndNext[E, A, B, D](d, OpticLang[E].gtVert(e, run(p)))
+
+  def landing3[E[_]: OpticLang, S, A, B](
+      e1: E[Getter[S, A]], 
+      e2: E[Getter[A, B]]): Unk[E, Getter[S, B]] =
+    Unk(OpticLang[E].gtVert(e1, e2))
+
+  implicit def pfOpticLang[E[_]](implicit ev: OpticLang[E]) = new OpticLang[ProductFirst[E, ?]] {
 
     def flVert[S, A, B](
         l: ProductFirst[E, Fold[S, A]], 
@@ -40,19 +61,17 @@ object ProductFirst {
         l: ProductFirst[E, Getter[S, A]], 
         r: ProductFirst[E, Getter[A, B]]): ProductFirst[E, Getter[S, B]] =
       (l, r) match {
-        case (First(), e) => FirstAndThen(run(e))
-        case (FirstAndThen(e1), e2) => 
-          FirstAndThen(OpticLang[E].gtVert(e1, run(e2)))
-        case (Product(e, _), First()) => Unk(e.asInstanceOf[E[Getter[S, B]]])
-        case (Product(e1, _), FirstAndThen(e2)) =>
-          Unk(OpticLang[E].gtVert(e1, e2))
+        case (First(d), e) => landing1(d, e)
+        case (FirstAndNext(d, e1), e2) => landing2(d, e1, e2)
+        case (Product(e, _), First(_)) => Unk(e)
+        case (Product(e1, _), FirstAndNext(_, e2)) => landing3(e1, e2)
         case _ => Unk(OpticLang[E].gtVert(run(l), run(r)))
       }
 
     def gtHori[S, A, B](
         l: ProductFirst[E, Getter[S, A]],
         r: ProductFirst[E, Getter[S, B]]): ProductFirst[E, Getter[S, (A, B)]] =
-      Product[E, S, A, B, (A, B)](run(l), run(r))
+      Product[E, S, A, B](run(l), run(r))
 
     def aflVert[S, A, B](
         l: ProductFirst[E, AffineFold[S, A]], 
@@ -78,7 +97,7 @@ object ProductFirst {
       Unk(OpticLang[E].equal)
 
     def first[A, B]: ProductFirst[E, Getter[(A, B), A]] = 
-      First[E, A, B, (A, B)]()
+      First(OpticLang[E].first)
 
     def second[A, B]: ProductFirst[E, Getter[(A, B), B]] = 
       Unk(OpticLang[E].second)
