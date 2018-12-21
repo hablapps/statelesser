cc41555f228d507f8462cd55111460301c39a0ad
Jesús López-González <jesus.lopez@hablapps.com>
2018-12-21 11:05:28 +0100

    Testing with regular expressions (to ignore var names)

--- sql/src/test/scala/CoupleExampleTest.scala
+++ sql/src/test/scala/CoupleExampleTest.scala
@@ -2,7 +2,8 @@ package statelesser
 package sql
 package test
 
-import scalaz.Const
+import scala.util.matching.Regex
+import scalaz._, Scalaz._
 import OpticLang.{Semantic, Table}
 import statelesser.test._
 import org.scalatest._
@@ -14,54 +15,59 @@ class CoupleExampleTest extends FlatSpec with Matchers {
 
   val keys = Map("Person" -> "name")
 
+  def syms(pattern: Stream[Char], i: Int): Stream[String] = 
+    pattern.zipWithL(Stream.continually(i)) { (c, oi) => 
+      s"""$c${oi.getOrElse("")}""" 
+    } #::: syms(pattern, i + 1)
+
   def genSql[S, A](sem: Semantic[Const[String, ?], Fold[S, A]]): String = {
     // println(sem)
     val (t, out) = sem(Table(Stream.range('a', 'z').map(_.toString), Map()))
     sqlToString(fromSemantic(t, out, keys))
   }
 
-  def matchSql[S, A](q: String, stks: Stack[Fold[S, A]]*) =
-    stks.map(stk => genSql(stk)) should contain theSameElementsAs 
-      List.fill(stks.length)(q)
+  def matchSql[S, A](r: Regex, stks: Stack[Fold[S, A]]*) =
+    stks.foreach { stk => 
+      genSql(stk) should fullyMatch regex r
+    } 
 
   "Statelesser" should "generate wildcard '*' selection" in {
-    matchSql("SELECT p.* FROM Person AS p;", getPeople)
+    matchSql(raw"SELECT (.)\.\* FROM Person AS \1;".r, getPeople)
   }
 
   it should "generate specific selection" in {
-    matchSql("SELECT p.name FROM Person AS p;", getPeopleName_1)
+    matchSql(raw"SELECT (.)\.name FROM Person AS \1;".r, getPeopleName_1)
   }
 
   it should "generate multi-selection" in {
     matchSql(
-      "SELECT p.name, p.age FROM Person AS p;", 
+      raw"SELECT (.)\.name, \1\.age FROM Person AS \1;".r, 
       getPeopleNameAndAge_1, getPeopleNameAndAge_2)
   }
 
   it should "generate wildcard nested selection" in {
     matchSql(
-      "SELECT w.* FROM Couple AS c INNER JOIN Person AS w ON c.her = w.name;", 
+      raw"SELECT (.)\.\* FROM Couple AS (.) INNER JOIN Person AS \1 ON \2\.her = \1\.name;".r, 
       getHer)
   }
 
   it should "generate nested specific selection" in {
     matchSql(
-      "SELECT w.name FROM Couple AS c INNER JOIN Person AS w ON c.her = w.name;",
+      raw"SELECT (.)\.name FROM Couple AS (.) INNER JOIN Person AS \1 ON \2\.her = \1\.name;".r,
       getHerName)
   }
 
   it should "generate nested multi-selection" in {
     matchSql(
-      "SELECT w.name, w.age FROM Couple AS c INNER JOIN Person AS w ON c.her = w.name;",
-      //getHerNameAndAge_1, 
+      raw"SELECT (.)\.name, \1\.age FROM Couple AS (.) INNER JOIN Person AS \1 ON \2\.her = \1\.name;".r,
+      getHerNameAndAge_1, 
       getHerNameAndAge_2, 
-      //getHerNameAndAge_3
-      )
+      getHerNameAndAge_3)
   }
 
   it should "generate multi-selection with literals" in {
     matchSql(
-      "SELECT p.name, 3 FROM Person AS p;",
+      raw"SELECT (.)\.name, 3 FROM Person AS \1;".r,
       getPeopleNameAnd3_1,
       getPeopleNameAnd3_2,
       getPeopleNameAnd3_3,
